afbb17ec25c6773f7d6bb98b35055919eadca21a
Jonatán Luzón Montaño <jonatan.luzon@e-campus.uab.cat>
2020-11-05 10:42:24 +0100

    creación del test del constructor de la clase tablero

---
+++ src/main/java/hundirlaflota/Tablero.java
@@ -0,0 +1,5 @@
+package hundirlaflota;
+
+public class Tablero {
+	
+}
---
+++ src/test/java/test/TableroTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import hundirlaflota.Tablero;
+
+class TableroTest {
+
+	@Test
+	void contructor() {
+		Tablero t = new Tablero();
+		for(int i = 0; i < 10; i++) {
+			for ( int j = 0; j < 10; j++) {
+				assertEquals(t.getTablero[0][0], 0);
+			}
+		}
+	}
+
+}
